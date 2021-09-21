7aaa5e72d7e0a0293a65e45b2980d397d7ef1fdf
Morion <adrian.martinez@udea.edu.co>
2021-09-21 14:19:53 -0500

    Globos que miran la cámara

--- Assets/_Game/Scripts/ElementosAbiente/PersonajeIniciador.cs
+++ Assets/_Game/Scripts/ElementosAbiente/PersonajeIniciador.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PersonajeIniciador : MonoBehaviour
 {
+    public GameObject camara;
     // Start is called before the first frame update
     void Start()
     {
         ControlModos.singleton.personaje = gameObject;
+        GlobosControl.singleton.camaraActiva = camara.transform;
     }
 
     // Update is called once per frame
